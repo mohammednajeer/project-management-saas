bdfc8c68a21717f06e36cbaeabd27bf8a5757c85
mohammednajeer <mohammednajeer.n@gmail.com>
2026-05-13 12:33:59 +0530

    updated workspace urls

--- backend/management/workspace/urls.py
+++ backend/management/workspace/urls.py
@@ -4,7 +4,7 @@ from .views import (
     WorkspaceDashboardView,
     WorkspaceTasksView,
     WorkspaceSubTasksView,
-    WorkspaceSubTaskStatusUpdateView,
+    WorkspaceSubTaskStatusUpdateView ,
 )
 
 urlpatterns = [
